e885baf65ce4222301a99e7bd016d1f61df0cc58
yds629 <youdongsen@163.com>
2016-07-15 19:15:44 +0800

    ustcjob3/4

---
+++ ustcjob4.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+ 
+url = 'http://job.ustc.edu.cn/list.php?MenuID=002004'
+front = 'http://job.ustc.edu.cn/'
+
+#查询count条记录
+post_data = {
+    'Referer': "http://job.ustc.edu.cn/list.php?MenuID=002004", 
+    'User-Agent':"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
+    } 
+return_data = requests.post(url, post_data)
+# print return_data.text
+soup = BeautifulSoup(return_data.text,'lxml')
+
+job1=soup.find_all(class_="pagerightText")[0]
+
+for job in job1.find_all('li'):
+    url = front + job.find('a').get('href')
+    name = job.find('a').get_text(strip=True)
+    time = job.find('span').get_text(strip=True)
+    # place = job.find(class_ = 'zhuanye').get_text(strip=True)
+    print name, time
+    print  url,'\n'
